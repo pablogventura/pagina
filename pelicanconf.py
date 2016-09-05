0d68e530c41848ff44f3b30ff4757598beb848ad
Pablo Ventura <pablogventura@gmail.com>
2016-09-05 00:47:28 -0300

    configurando

---
+++ pelicanconf.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Pablo Ventura'
+SITENAME = u'Pablo Ventura - FaMAF'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Argentina/Cordoba'
+
+DEFAULT_LANG = u'es'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+#LINKS = (('Wikipedia', 'http://www.wikipedia.org/'),)
+
+# Social widget
+SOCIAL = (('GitHub', 'https://github.com/pablogventura/'),
+          ("Wikipedia", "https://es.wikipedia.org/wiki/Usuario:"),)
+
+DEFAULT_PAGINATION = 10
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
